aa524d76717ebff7846c008c0e9680f83101517f
Emilio Castillo <rawsecsolutions@gmail.com>
2026-01-26 15:19:58 -0300

    config.js

---
+++ js/config.js
@@ -0,0 +1,4 @@
+// js/config.js
+const SUPABASE_URL = 'https://wrmelwftwumsfwzjjoxa.supabase.co';
+const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndybWVsd2Z0d3Vtc2Z3empqb3hhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjkzODM3NjUsImV4cCI6MjA4NDk1OTc2NX0.upHE7sTVioEYsccEFmr4bqDxS__UuT-uXzqUJozNqd0';
+export { SUPABASE_URL, SUPABASE_KEY };
